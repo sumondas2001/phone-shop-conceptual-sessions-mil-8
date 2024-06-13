12ab4796f119a4e784224d018886b0e9b92759c8
sumondas2001 <sumonchandradas1010@gmail.com>
2024-06-14 04:15:23 +0600

    all completes

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -3,7 +3,7 @@
 const Banner = () => {
      return (
           <div className="max-w-6xl mx-auto  my-10 ">
-               <img className="h-[60vh] w-full rounded-xl" src={'../../../public/img/tech-daily-UODNyThs0vg-unsplash.jpg'} alt="" />
+               <img className="h-[60vh] w-full rounded-xl" src={'/img/tech-daily-UODNyThs0vg-unsplash.jpg'} alt="" />
           </div>
      );
 };
